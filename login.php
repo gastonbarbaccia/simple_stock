81e665de1e062631728e39543e03d2d03f9dc854
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-07-06 15:20:53 -0300

    ok

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php
 // checking for minimum PHP version
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+	exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
-    // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
-    // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-    require_once("libraries/password_compatibility_library.php");
+	// if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+	// (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+	require_once("libraries/password_compatibility_library.php");
 }
 
 // include the configs / constants for the database connection
@@ -20,77 +20,78 @@ $login = new Login();
 
 // ... ask if we are logged in here:
 if ($login->isUserLoggedIn() == true) {
-    // the user is logged in. you can do whatever you want here.
-    // for demonstration purposes, we simply show the "you are logged in" view.
-   header("location: stock.php");
-
+	// the user is logged in. you can do whatever you want here.
+	// for demonstration purposes, we simply show the "you are logged in" view.
+	header("location: stock.php");
 } else {
-    // the user is not logged in. you can do whatever you want here.
-    // for demonstration purposes, we simply show the "you are not logged in" view.
-    ?>
+	// the user is not logged in. you can do whatever you want here.
+	// for demonstration purposes, we simply show the "you are not logged in" view.
+?>
 	<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-  <title>Simple Stock | Login</title>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-  <!-- CSS  -->
-   <link href="css/login.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-   <link rel=icon href='img/logo-icon.png' sizes="32x32" type="image/png">
-</head>
-<body style="background-image: url('img/login.png'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
+	<html lang="es">
 
-  <!-- Start of the login form -->
- <div class="container">
-        <div class="card card-container">
-            <h1 class="text-center">Simple Stock</h1>
-			<hr>
-            <p id="profile-name" class="profile-name-card"></p>
-            <form method="post" accept-charset="utf-8" action="login.php" name="loginform" autocomplete="off" role="form" class="form-signin">
-			<?php
-				// show potential errors / feedback (from login object)
-				if (isset($login)) {
-					if ($login->errors) {
-						?>
-						<div class="alert alert-danger alert-dismissible" role="alert">
-						    <strong>Error!</strong> 
-						
-						<?php 
-						foreach ($login->errors as $error) {
-							echo $error;
-						}
-						?>
-						</div>
-						<?php
-					}
-					if ($login->messages) {
-						?>
-						<div class="alert alert-success alert-dismissible" role="alert">
-						    <strong>Aviso!</strong>
-						<?php
-						foreach ($login->messages as $message) {
-							echo $message;
-						}
-						?>
-						</div> 
-						<?php 
-					}
-				}
-				?>
-                <span id="reauth-email" class="reauth-email"></span>
-                <input class="form-control" placeholder="Usuario" name="user_name" type="text" value="" autofocus="" required>
-                <input class="form-control" placeholder="Contraseña" name="user_password" type="password" value="" autocomplete="off" required>
-                <button type="submit" class="btn btn-lg btn-success btn-block btn-signin" name="login" id="submit">Iniciar Sesión</button>
-            </form><!-- /form -->
-            
-        </div><!-- /card-container -->
-    </div><!-- /container -->
-  </body>
-</html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+		<title>Simple Stock | Login</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<!-- CSS  -->
+		<link href="css/login.css" type="text/css" rel="stylesheet" media="screen,projection" />
+		<link rel=icon href='img/logo-icon.png' sizes="32x32" type="image/png">
+	</head>
 
-	<?php
+	<body style="background-image: url('img/login.png'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
+
+		<!-- Start of the login form -->
+		<div class="container">
+			<div class="card card-container">
+				<div><h1 class="text-center"> <img src="./img/logo-icon.png" style="width: 50px;height:50px">Simple Stock</h1></div>
+				
+				<hr>
+				<p id="profile-name" class="profile-name-card"></p>
+				<form method="post" accept-charset="utf-8" action="login.php" name="loginform" autocomplete="off" role="form" class="form-signin">
+					<?php
+					// show potential errors / feedback (from login object)
+					if (isset($login)) {
+						if ($login->errors) {
+					?>
+							<div class="alert alert-danger alert-dismissible" role="alert">
+								<strong>Error!</strong>
+
+								<?php
+								foreach ($login->errors as $error) {
+									echo $error;
+								}
+								?>
+							</div>
+						<?php
+						}
+						if ($login->messages) {
+						?>
+							<div class="alert alert-success alert-dismissible" role="alert">
+								<strong>Aviso!</strong>
+								<?php
+								foreach ($login->messages as $message) {
+									echo $message;
+								}
+								?>
+							</div>
+					<?php
+						}
+					}
+					?>
+					<span id="reauth-email" class="reauth-email"></span>
+					<input class="form-control" placeholder="Usuario" name="user_name" type="text" value="" autofocus="" required>
+					<input class="form-control" placeholder="Contraseña" name="user_password" type="password" value="" autocomplete="off" required>
+					<button type="submit" class="btn btn-lg btn-success btn-block btn-signin" name="login" id="submit">Iniciar Sesión</button>
+				</form><!-- /form -->
+
+			</div><!-- /card-container -->
+		</div><!-- /container -->
+	</body>
+
+	</html>
+
+<?php
 }
-
-
